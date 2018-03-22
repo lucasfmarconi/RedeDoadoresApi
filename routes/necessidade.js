6ce9f599eef9923e83b092a7a3e343ee9c6075a2
Lucas F Marconi Ferreira <lucasfmarconi@gmail.com>
2018-03-21 22:53:11 -0300

    Changing necessidade route

--- routes/necessidade.js
+++ routes/necessidade.js
@@ -1,12 +1,22 @@
 var necessidade = require("../api/necessidade/necessidadeLogic");
 
 var appRouter = function (app) {
+    
     app.get("/api/necessidade", function (req, res) {
         getNecessidades(req, res);
     });
 
     async function getNecessidades(req, res) {
         var data = await necessidade.getNecessidades();
+        res.status(200).send(data);
+    }
+
+    app.post("/api/necessidade", function (req, res) {
+        postNecessidade(req, res);
+    });
+
+    async function postNecessidade(req, res) {
+        var data = await necessidade.postNecessidade(req);
         res.status(200).send(data);
     }
 }
