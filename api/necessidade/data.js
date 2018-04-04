9b2f7bca41b115047ea08ff8502e30f6b196e7e3
Lucas F Marconi Ferreira <lucas.ferreira@vetta.com.br>
2018-04-04 16:52:20 -0300

    Changing necessidade api

--- api/necessidade/data.js
+++ api/necessidade/data.js
@@ -1,7 +1,14 @@
 var db = require("../../db/postgres/index");
 
 async function get() {
-    return await db.clientSelect(`SELECT id, descricao, nome, tipo_sanguineo, hospital FROM public.rdapi_necessidade`);
+    return await db.clientSelect(`SELECT 
+                                    id,
+                                    descricao,
+                                    nome,
+                                    tipo_sanguineo,                                    
+                                    hospital 
+                                    FROM 
+                                    public.rdapi_necessidade`);
 }
 
 module.exports = {
